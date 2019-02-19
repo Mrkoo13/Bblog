dbf2bb3f51e35b652180d050b38ce8479d46846f
miha <mihailgrednev390@gmail.com>
2019-02-19 14:12:10 +0000

    Bblog

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'posts#index', as:'home'
+
+   get 'about' =>  'pages#about', as:'about'
+
+   get 'new' =>  'pages#new'
+
+
+ resources :posts do
+   resources :comments
+end
 end
